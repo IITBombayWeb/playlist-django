c88b48c72dd89ed5ac4eca6d0ba9e457e817390a
Subhasree Mondal <shubhasree97@gmail.com>
2022-10-31 11:27:09 +0530

    Delete playlist directory

--- playlist/plydjango/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from plydjango.models import Song, SongAdmin
-
-admin.site.register(Song,SongAdmin)
--- playlist/plydjango/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlydjangoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'plydjango'
